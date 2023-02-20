08ebef2ad2dbd295455de99d87b287f3fc5a3f7c
Toshiba <Toshiba@DESKTOP-B345MNB>
2023-02-20 18:16:38 +0300

    Proje dosyası ekle.

---
+++ PythonApplication2/PythonApplication2.py
@@ -0,0 +1,8 @@
+x=input("x degerini girin: ")
+y=input("y degerini girin: ")
+if (x<y):
+    print("x degeri y den buyuk")
+elif (x==y):
+    print("x ve y degeri esit")
+if (y<x):
+    print("y degeri x den buyuk")
